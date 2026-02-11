d309bb430e2f53b9f0c8b31ac6ec5c5f7a6bce69
Rafael Serafim <rafaelserafimcalzzetta@gmail.com>
2026-02-11 11:27:30 -0300

    arquivo descompactado instalado

---
+++ 1_2026/src/main/java/com/aula1_2026/__2026/Application.java
@@ -0,0 +1,13 @@
+package com.aula1_2026.__2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ 1_2026/src/test/java/com/aula1_2026/__2026/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula1_2026.__2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
